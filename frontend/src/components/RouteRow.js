9ed34f68122e1e3da750c54839c577c1bb323fd4
Andy <andy.ward.uk@gmail.com>
2020-01-04 21:47:50 +0000

    noopener, noreferrer on link

--- frontend/src/components/RouteRow.js
+++ frontend/src/components/RouteRow.js
@@ -7,14 +7,32 @@ export default class RouteRow extends Component {
   render() {
     return (
       <tr>
-        <td><a href={`http://www.strava.com/routes/${this.props.route.routeid}`} target='_blank'>{this.props.route.routeid}</a></td>
+        <td>
+          <a
+            href={`http://www.strava.com/routes/${this.props.route.routeid}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            {this.props.route.routeid}
+          </a>
+        </td>
         <td>{this.props.route.name}</td>
         <td>{this.props.route.description}</td>
-        <td className='text-right text-nowrap'><Distance mt={this.props.route.distance} unit='mi' dp='1'/></td>
-        <td className='text-right text-nowrap'><Distance mt={this.props.route.distance} unit='km' dp='1'/></td>
-        <td className='text-right text-nowrap'><Distance mt={this.props.route.elevation_gain} unit='mt' dp='0'/></td>
-        <td className='text-right text-nowrap'><Distance mt={this.props.route.elevation_gain} unit='ft' dp='0'/></td>
-        <td className='text-right text-nowrap'><TimeSpan secs={this.props.route.estimated_moving_time} minUnit='m' /></td>
+        <td className='text-right text-nowrap'>
+          <Distance mt={this.props.route.distance} unit='mi' dp='1'/>
+        </td>
+        <td className='text-right text-nowrap'>
+          <Distance mt={this.props.route.distance} unit='km' dp='1'/>
+        </td>
+        <td className='text-right text-nowrap'>
+          <Distance mt={this.props.route.elevation_gain} unit='mt' dp='0'/>
+        </td>
+        <td className='text-right text-nowrap'>
+          <Distance mt={this.props.route.elevation_gain} unit='ft' dp='0'/>
+        </td>
+        <td className='text-right text-nowrap'>
+          <TimeSpan secs={this.props.route.estimated_moving_time} minUnit='m' />
+        </td>
       </tr>
     )
   }
