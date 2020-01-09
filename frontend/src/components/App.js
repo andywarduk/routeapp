44d1be88ece5b7ec23aa24a7711d95f593dd7a4d
Andy <andy.ward.uk@gmail.com>
2020-01-09 06:37:43 +0000

    Icons

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import FilteredRouteTable from './FilteredRouteTable'
-import { Container, Navbar, NavbarBrand } from 'reactstrap'
+import { Container, Navbar, NavbarBrand, NavItem } from 'reactstrap'
 
 function App() {
   var devMsg
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <Navbar dark={true} color='primary'>
-        <NavbarBrand>CCC Route Finder{devMsg}</NavbarBrand>
+        <NavbarBrand><img src='/logow32.png'/>&nbsp;&nbsp;CCC Route Finder{devMsg}</NavbarBrand>
       </Navbar>
       <Container fluid>
         <FilteredRouteTable/>
