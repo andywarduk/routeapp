c9331b9791f8735baeab8b4fc6b9313a75fa36a9
Andy <andy.ward.uk@gmail.com>
2020-01-31 16:34:55 +0000

    Backend eslint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,27 +1,25 @@
-export default {
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
     "extends": [
         "eslint:recommended",
-        "plugin:node/recommended",
         "plugin:@typescript-eslint/eslint-recommended"
     ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "ecmaVersion": 2020,
+        "ecmaVersion": 2018,
         "sourceType": "module"
     },
     "plugins": [
         "@typescript-eslint"
     ],
     "rules": {
-        "node/exports-style": ["error", "module.exports"],
-        "node/file-extension-in-import": ["error", "always"],
-        "node/prefer-global/buffer": ["error", "always"],
-        "node/prefer-global/console": ["error", "always"],
-        "node/prefer-global/process": ["error", "always"],
-        "node/prefer-global/url-search-params": ["error", "always"],
-        "node/prefer-global/url": ["error", "always"],
-        "node/prefer-promises/dns": "error",
-        "node/prefer-promises/fs": "error",
         "no-var": 2,
         "@typescript-eslint/no-unused-vars": 2
     }
